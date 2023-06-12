54aa93fb989ec62f16a6fabb90bcf0eda97f0317
carlowil <matveysizov84@gmail.com>
2023-06-12 22:09:20 +0300

    state in compose

---
+++ app/src/main/java/com/carlowil/first_compose/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.carlowil.first_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.carlowil.first_compose.ui.theme.First_ComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            First_ComposeTheme {
+                // A surface container using the 'background' color from the theme
+                MyApp(modifier = Modifier.fillMaxSize())
+            }
+        }
+    }
+}
+@Composable
+private fun MyApp(modifier: Modifier = Modifier, 
+                  names : List<String> = listOf("World", "Compose")) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+@Composable
+fun Greeting(name: String) {
+    val expanded = remember{ mutableStateOf(false)}
+    val extraPadding = if(expanded.value) 48.dp else 0.dp
+
+    Surface(color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
+                Text(text = "Hello,")
+                Text(text = "$name!")
+            }
+            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    First_ComposeTheme {
+        Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun MyAppPreview() {
+    First_ComposeTheme {
+        MyApp()
+    }
+}
